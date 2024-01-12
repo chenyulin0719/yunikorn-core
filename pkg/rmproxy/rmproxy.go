872158fdbbfed6a4ee4e71064ad9bc96461097d1
Yu-Lin Chen <kh87313@gmail.com>
2024-01-12 12:59:40 +0800

    Add delay to trigger race condition

--- pkg/rmproxy/rmproxy.go
+++ pkg/rmproxy/rmproxy.go
@@ -23,6 +23,7 @@ import (
 	"reflect"
 	"strconv"
 	"sync"
+	"time"
 
 	"go.uber.org/zap"
 
@@ -387,6 +388,12 @@ func (rmp *RMProxy) UpdateNode(request *si.NodeRequest) error {
 				log.Log(log.SchedNode).Info(fmt.Sprintf("### in RMProxy UpdateNode, %v,%v", node.NodeID, node.GetOccupiedResource()))
 			}
 		}
+
+		if request.Nodes != nil && request.Nodes[0] != nil && request.Nodes[0].OccupiedResource != nil && request.Nodes[0].OccupiedResource.Resources != nil && request.Nodes[0].OccupiedResource.Resources["pods"] != nil && request.Nodes[0].OccupiedResource.Resources["pods"].Value == 1 {
+			log.Log(log.RMProxy).Info(fmt.Sprintf("### Sleep 2 sec...  %v", request))
+			time.Sleep(time.Duration(2) * time.Second)
+		}
+
 		rmp.EventHandlers.SchedulerEventHandler.HandleEvent(&rmevent.RMUpdateNodeEvent{Request: request})
 	}()
 	return nil
